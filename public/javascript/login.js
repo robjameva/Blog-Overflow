f17a91c810d63f4d859e4ccfc8c431578af30047
Robert Evanik <rob.evanik@gmail.com>
2022-01-29 09:20:53 -0500

    Updated nav bar to include home, added margin to nav btns, fixed dashbaord username bug

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -40,7 +40,7 @@ async function signupFormHandler(event) {
             headers: { 'Content-Type': 'application/json' }
         })
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText)
         }
